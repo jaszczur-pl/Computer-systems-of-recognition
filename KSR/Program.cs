a1c257d99b1c1a7206167a4cae9ad269710fffb2
mjaszczura <maciej.jaszczura17@gmail.com>
2019-03-19 00:11:18 +0100

    Added SGMLParser

--- KSR/Program.cs
+++ KSR/Program.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace KSR
 {
     class Program
     {
         static void Main(string[] args) {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            SGMLParser sgmlParser = new SGMLParser();
+            XmlDocument xmlDoc = sgmlParser.GetXmlDoc();
+            
+            Console.WriteLine(xmlDoc.ParentNode);
         }
     }
 }
---
+++ KSR/SGMLParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Sgml;
+
+namespace KSR
+{
+    class SGMLParser
+    {
+        private const string filePath = "E:\\Studia - informatyka\\6 semestr\\KSR\\reuters21578\\reut2-000.sgm";
+        private const string dtdPath = "E:\\Studia - informatyka\\6 semestr\\KSR\\reuters21578\\lewis.dtd";
+
+        public XmlDocument GetXmlDoc() {
+            TextReader reader = new StreamReader(filePath);
+
+            SgmlReader sgmlReader = new SgmlReader();
+            sgmlReader.IgnoreDtd = true;
+            //sgmlReader.SystemLiteral = dtdPath;
+            sgmlReader.WhitespaceHandling = WhitespaceHandling.All;
+            sgmlReader.InputStream = reader;
+
+            //create XML document
+            //List<XmlDocument> xmlDocList = new List<XmlDocument>();
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.PreserveWhitespace = true;
+                xmlDoc.XmlResolver = null;
+                xmlDoc.Load(sgmlReader);
+
+                //xmlDocList.Add(xmlDoc);
+
+
+            return xmlDoc;
+        }
+
+        /*public void readFile() {
+            try {
+                string line = null;
+                TextReader readFile = new StreamReader(V);
+                while (true) {
+                    line = readFile.ReadLine();
+                    if (line != null) {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException ex) {
+                Console.WriteLine(ex.ToString());
+            }
+        } */
+
+    }
+}
