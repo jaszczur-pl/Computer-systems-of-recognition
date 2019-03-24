3c408c988cf74038d2d11a126df2125ed2fcf80e
Maciej Jaszczura <maciej.jaszczura17@gmail.com>
2019-03-24 19:31:37 +0100

    Added 2 reuters classifications:
    a) classification by places tag
    b) classifciation by topics tag

--- KSR/Program.cs
+++ KSR/Program.cs
@@ -12,10 +12,14 @@ namespace KSR
     {
         static void Main(string[] args) {
 
+            //west-germany, usa, france, uk, canada, japan 
+
             string[] allFilePaths = Directory.GetFiles("C:\\Users\\maciej.jaszczura\\OneDrive - Accenture\\!Sync\\Downloads\\reuters21578.tar\\", "*sgm");
             FileHandler fileHandler = new FileHandler();
             SgmlToXmlParser sgmlParser = new SgmlToXmlParser();
             XmlDocument xmlDoc = sgmlParser.GetXmlDoc(allFilePaths[0]);
+
+            //omit first file path in array for further processing
             allFilePaths = allFilePaths.Where(x => x != allFilePaths[0]).ToArray();
 
             foreach (string singleFilePath in allFilePaths) {
@@ -29,11 +33,31 @@ namespace KSR
             }
 
             XmlNode xmlRoot = xmlDoc.DocumentElement;
-            int xmlNodes = xmlRoot.ChildNodes.Count;
-            XmlNode firstXmlNode = xmlRoot.ChildNodes.Item(1999);
+            XmlNodeList nodeListPlacesUsa = xmlRoot.SelectNodes("REUTERS[PLACES/D='usa' and PLACES[count(D)=1]]");
+            XmlNodeList nodeListPlacesWestGermany = xmlRoot.SelectNodes("REUTERS[PLACES/D='west-germany' and PLACES[count(D)=1]]");
+            XmlNodeList nodeListPlacesFrance = xmlRoot.SelectNodes("REUTERS[PLACES/D='france' and PLACES[count(D)=1]]");
+            XmlNodeList nodeListPlacesUk = xmlRoot.SelectNodes("REUTERS[PLACES/D='uk' and PLACES[count(D)=1]]");
+            XmlNodeList nodeListPlacesCanada = xmlRoot.SelectNodes("REUTERS[PLACES/D='canada' and PLACES[count(D)=1]]");
+            XmlNodeList nodeListPlacesJapan = xmlRoot.SelectNodes("REUTERS[PLACES/D='japan' and PLACES[count(D)=1]]");
 
-            Console.WriteLine(firstXmlNode.OuterXml);
-            Console.WriteLine(xmlNodes);
+            XmlNodeList nodeListTopicsEarn = xmlRoot.SelectNodes("REUTERS[TOPICS/D='earn' and TOPICS[count(D)=1]]");
+            XmlNodeList nodeListTopicsInterest = xmlRoot.SelectNodes("REUTERS[TOPICS/D='interest' and TOPICS[count(D)=1]]");
+            XmlNodeList nodeListTopicsCoffee = xmlRoot.SelectNodes("REUTERS[TOPICS/D='coffee' and TOPICS[count(D)=1]]");
+            XmlNodeList nodeListTopicsMoneyFx = xmlRoot.SelectNodes("REUTERS[TOPICS/D='money-fx' and TOPICS[count(D)=1]]");
+            XmlNodeList nodeListTopicsCrude = xmlRoot.SelectNodes("REUTERS[TOPICS/D='crude' and TOPICS[count(D)=1]]");
+
+            int a = nodeListTopicsEarn.Count;
+            int b = nodeListTopicsInterest.Count;
+            int c = nodeListTopicsCoffee.Count;
+            int d = nodeListTopicsCrude.Count;
+            int e = nodeListTopicsMoneyFx.Count;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(nodeListTopicsEarn.Item(0).OuterXml);
         }
     }
 }
