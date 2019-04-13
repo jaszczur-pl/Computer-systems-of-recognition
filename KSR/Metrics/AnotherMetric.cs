f2cd3b7d87745c477119409c9b98009de148dca6
Maciej Jaszczura <maciej.jaszczura17@gmail.com>
2019-04-13 20:30:35 +0200

    Added MinMax metric

---
+++ KSR/Metrics/AnotherMetric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSR.Metrics
+{
+    class AnotherMetric 
+    {
+        public double CalculateDistance(Article a, Article b) {
+            double distance = 0;
+
+
+            return distance;
+        }
+    }
+}
---
+++ KSR/Metrics/MinMaxMetric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSR.Metrics
+{
+    class MinMaxMetric : IMetric
+    {
+        //metryka działa podobnie jak uliczna z tym, że odrzucana jest najmniejsza i największa wartość odległości pomiędzy pojedynczymi cechami
+
+        public double CalculateDistance(List<double> appointedAttributes, List<double> testedAttributes) {
+            double distance = 0;
+            List<double> distancesList = new List<double>();
+
+            for (int i = 0; i < appointedAttributes.Count; i++) {
+                distancesList.Add(Math.Abs((appointedAttributes.ElementAt(i) - testedAttributes.ElementAt(i))));
+                distance += Math.Abs((appointedAttributes.ElementAt(i) - testedAttributes.ElementAt(i)));
+            }
+
+            double max = distancesList.Max();
+            double min = distancesList.Min();
+
+            distance = distance - max - min;
+
+            return distance;
+        }
+    }
+}
